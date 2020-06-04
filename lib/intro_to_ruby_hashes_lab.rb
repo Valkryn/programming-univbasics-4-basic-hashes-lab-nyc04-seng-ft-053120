7803371a47fb8003aefbbc2ffac80b931f7ce6c3
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-04 13:56:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,16 @@ def new_hash
 end
 
 def my_hash
-  greek_myth = {Zeus: "Thunder"}
+  greek_myth = {Zeus: "King of Gods"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  test = [name: "Grace Hopper"]
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  test = [ id: number]
 end
