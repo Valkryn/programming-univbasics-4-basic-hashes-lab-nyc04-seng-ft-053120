f94bc925e4b0b58690c3503f1b4bf3b13418ad3e
Calvin Gutierrez <calvinraphaelg@gmail.com>
2020-06-04 14:00:32 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,19 @@
 def new_hash
-  # return an empty hash
+  greek_myth = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  greek_myth = {zeus: "King of Gods"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = { name: "Grace Hopper"}
+  return pioneer
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  test = { id: number}
 end
